7c21185cc3ed4e09cf93c20e0d1561b275d8a7b0
Iain Emsley <iain.emsley@warwick.ac.uk>
2022-05-18 11:50:22 +0100

    Removed unuese imports and fixed comment

--- app/src/main/java/uk/ac/warwick/cim/unheardCity/WifiDetails.java
+++ app/src/main/java/uk/ac/warwick/cim/unheardCity/WifiDetails.java
@@ -1,25 +1,11 @@
 package uk.ac.warwick.cim.unheardCity;
 
-import android.annotation.SuppressLint;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.pm.PackageManager;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
-import android.net.wifi.rtt.RangingRequest;
-import android.net.wifi.rtt.RangingResult;
-import android.net.wifi.rtt.RangingResultCallback;
-import android.net.wifi.rtt.WifiRttManager;
-import android.os.Build;
 import android.util.Log;
-
-import androidx.annotation.RequiresApi;
 
 import java.io.File;
 import java.util.List;
-import java.util.concurrent.Executor;
 
 
 public class WifiDetails {
@@ -41,14 +27,13 @@ public class WifiDetails {
 
             }
         }  catch (Exception e ) {
-            System.out.print(e);
+            Log.i("WiFi", e.getMessage());
         }
     }
 
     public void scanFailure() {
         // handle failure: new scan did NOT succeed
-        // consider using old scan results: these are the OLD results!
-        //List<ScanResult> results = wifiManager.getScanResults();
+        // Provide a message
         Log.i("WIFI", "Scan wifi failed");
 
     }
